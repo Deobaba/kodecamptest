3fe131725dec1f6f8ffc3bbbb24d3864a94ae329
Deobaba <adexsquare4192@gmail.com>
2023-07-28 23:27:12 +0100

    add /api/user route

--- server.js
+++ server.js
@@ -4,21 +4,34 @@ const http = require('http')
 
 PORT = process.env.PORT || 3000
 
+const userInfo = {
+    name : "Adenekan Adeola",
+    emnail: "adexsquare4192@gmail.com",
+    age: 27
+}
+
+let tobject = JSON.stringify(userInfo)
+console.log(tobject)
 const server = http.createServer((req,res)=>{
     // console.log(req.url)
     if(req.url==='/file'){
-        res.statusCode = 200
+    res.statusCode = 200
     res.setHeader("content-type","text/plain")
     const first = readFileSync('data.txt','utf8')
     res.end(`${first}`)
+    return
     }
-    else{
-        res.statusCode = 200
-        res.setHeader("content-type","text/plain")
-        res.end("Hello,Nodejs!")
-    }
-   
 
+    if(req.url ==='/api/user'){
+        res.statusCode = 200
+        res.setHeader("content-type","application/json")
+        res.write(JSON.stringify(userInfo))
+        res.end()
+    }
+        res.statusCode = 200
+        res.setHeader("content-type","application/json")
+        res.end("Hello,Nodejs!")
+        console.log('it got here regardless')
 })
 
 //  const filedeo = readFile('data.txt','utf8',(err,result)=>{
